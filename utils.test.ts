a37ae3d4142fcbd3da666d924e2d41a8069ac464
Uri Klar <uriklar@gmail.com>
2021-04-30 09:00:49 +0300

    tests + what's new

--- utils.test.ts
+++ utils.test.ts
@@ -1,10 +1,17 @@
-import {getNextFreeId} from "./utils"
+import { getNextFreeId } from "./utils";
 
 describe("getNextFreeId", () => {
   it("should find next id correctly", () => {
     expect(getNextFreeId("test", ["test-1", "test-3"])).toEqual("2");
 
-    expect(getNextFreeId("test-uri", ["test-uri-1", "test-2", "test-3", "test-uri-2", "test-uri-3"])).toEqual("4");
-
+    expect(
+      getNextFreeId("test-uri", [
+        "test-uri-1",
+        "test-2",
+        "test-3",
+        "test-uri-2",
+        "test-uri-3",
+      ])
+    ).toEqual("4");
   });
 });
